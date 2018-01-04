f97ce3d18ace4d71bfcb78b68793e873982e7743
ave <artan.veliju@futuretek.ch>
2018-01-04 15:10:25 +0100

    update

--- compiler.go
+++ compiler.go
@@ -3,14 +3,30 @@ package less
 import (
 	"os/exec"
 	"bytes"
+	"fmt"
 )
 
-func Compile(path string, w *bytes.Buffer)error {
-	dateCmd := exec.Command("lessc", path)
-	dateOut, err := dateCmd.Output()
+func Compile(path string, w *bytes.Buffer, paths...string)error {
+	var cmd *exec.Cmd
+	l := len(paths)
+	if paths != nil && l > 0 {
+		pathBuffer := new(bytes.Buffer)
+		for i, p := range paths {
+			pathBuffer.WriteString(p)
+			if i >= 0 && l-1 != i {
+				pathBuffer.WriteString(":")
+			}
+		}
+		cmd = exec.Command("lessc", path, "--paths", pathBuffer.String())
+	} else {
+		cmd = exec.Command("lessc", path)
+	}
+	cmdOut, err := cmd.Output()
 	if err != nil {
+		fmt.Println(err)
+		fmt.Println(cmdOut)
 		return err
 	}
-	_, err = w.Write(dateOut)
+	_, err = w.Write(cmdOut)
 	return err
 }
